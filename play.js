01a576fa0495654e9d8b425ef6a3937f242e5592
Amir <amir.a.23@hotmail.com>
2021-11-03 18:48:52 +0000

    created client js module, used ES6 shorthand and sent name to server

--- play.js
+++ play.js
@@ -1,30 +1,6 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',// IP address here,
-    port: '50541'// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-
-  conn.on("connect", () => {
-    console.log("Your Connected!!!")
-    // code that does something when the connection is first established
-  });
-  
-  conn.on("data", (data) => {
-    console.log(data);
-    // code that receives data from server
-  });
-
-  return conn;
-};
-
+const connect = require("./client")
+//console.log(connect);
 console.log("Connecting ...");
 
 
-connect();
+connect.connect();
