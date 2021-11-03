030238d3b22854d90e2de3ed1222222766c7a80a
Amir <amir.a.23@hotmail.com>
2021-11-03 22:53:41 +0000

    created input file for user input functions and added the module to play.js

--- play.js
+++ play.js
@@ -1,29 +1,8 @@
-const connect = require("./client")
-
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 //console.log(connect);
 console.log("Connecting ...");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  //add event listeners before returning stdin
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-
-const handleUserInput = function (key) {
-  // your code here
-  console.log(key);
-  if (key === '\u0003') {
-    process.exit();
-  }
-
-};
-connect.connect();
+connect();
 
 setupInput(); //call after calling connect??
