daf94362c87183c3506258bdd7f7b13cbf15209b
Amir <amir.a.23@hotmail.com>
2021-11-03 21:20:47 +0000

    tested setTimeout and SetInterval to move snake

--- client.js
+++ client.js
@@ -14,9 +14,18 @@ const connect = () => {
   conn.on("connect", () => {
     console.log("Your Connected!!!")
     conn.write('Name: AMA');
+   // setInterval(() => {conn.write('Move: up')}, 1000);
+
+    //setTimeout(() => {conn.write('Move: up')}, 2000);
+    //setTimeout(() => {conn.write('Move: up')}, 3000);
+    //setTimeout(() => {conn.write('Move: up')}, 4000);
+    //setTimeout(() => {conn.write('Move: up')}, 5000);
+
+    //conn.write('Move: up');
     // code that does something when the connection is first established
   });
   
+
   conn.on("data", (data) => {
     console.log(data);
     // code that receives data from server
